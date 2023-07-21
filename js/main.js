d85719420bd9a8fef821dd2576cfe56c17ab92af
bdegan2016@gmail.com <bdegan2016@gmail.com>
2023-07-21 13:34:02 -0400

    final

--- js/main.js
+++ js/main.js
@@ -24,9 +24,9 @@ form.addEventListener("submit", (evento) => {
         
         atualizaElemento(itemAtual)
 
-        itens[existe.id] = itemAtual
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
     } else {
-        itemAtual.id = itens.length
+        itemAtual.id = itens[itens.length - 1] ? (item[itens.length - 1]).id + 1 : 0
 
         criaElemento(itemAtual)
 
@@ -49,10 +49,27 @@ function criaElemento(item) {
     novoItem.appendChild(numeroItem)
     
     novoItem.innerHTML += item.nome
+    novoItem.appendChild(botaoDeleta())
 
     lista.appendChild(novoItem)
 }
 
 function atualizaElemento(item) {
     document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
+
+function botaoDeleta(id){
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerHTML = "x"
+    elementoBotao.addEventListener("click", function(){
+        deletaElemento(this.parentNode, id)
+    } )
+
+    return elementoBotao
+}
+
+function deletaElemento(tag, id){
+    tag.remove()
+    itens.splice(item.findIndex(elemento => elemento.id == id), 1)
+    localStorage.setItem("itens",JSON.stringify(itens))
 }
